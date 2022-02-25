23a1d53c42654ba8f72446ab5b2283ddc8ee33fd
parkyoungmean <PYM@bag-yeongmin-ui-MacBook-Pro.local>
2022-02-25 18:21:09 +0900

    4-03th commit

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,39 +1,110 @@
 <template>
   <div class="grid place-items-center h-screen">
-    <!-- Profile card -->
-    <div class="w-full md:w-[400px] group cursor-pointer active:opacity-70 transition-opacity duration-300">
-      <!-- Title card -->
-      <div class="relative group bg-black rounded-t h-[50px] group-hover:h-[100px] transition-all duration-300">
-        <img src="../assets/profile/bg.jpeg" class="w-full h-full object-cover opacity-40 group-hover:opacity-60 transition duration-300" alt="">
-        <img
-          src="../assets/profile/학교장님.png"
-          class="
-            object-cover
-            absolute
-            w-[160px]
-            bottom-0
-            opacity-0
-            group-hover:opacity-100
-            transform
-            translate-y-3/4
-            group-hover:translate-y-0
-            transition
-            duration-500
-          "
-          alt=""
-        />
-        <h1 class="absolute top-1/2 transform -translate-y-1/2 left-5 group-hover:left-52 text-white font-bold text-md md:text-2xl transition-all duration-300">
-        장원혁 학교장
+    <div class="containter mx-auto md:px-12 lg:px-8">
+      <div class="mb-28 space-y-4 text-center">
+        <h1 class="text-4xl font-bold text-gray-900 md:text-5xl">
+          학교장님 소개
         </h1>
       </div>
-
-      <!-- Body Card -->
-      <div class="min-h-[120px] py-3 px-4 bg-white rounded-b relative z-10 shadow text-gray-600">
-        <div class="font-semibold mb-1">
-          <p>순전한 교회 담임목사</p>
-          <p>Email: pure@church.kr</p>
+      <!-- Profile card -->
+      <div
+        class="
+          w-full
+          md:w-[400px]
+          group
+          cursor-pointer
+          active:opacity-70
+          transition-opacity
+          duration-300
+        "
+      >
+        <!-- Title card -->
+        <div
+          class="
+            relative
+            group
+            bg-black
+            rounded-t
+            h-[50px]
+            group-hover:h-[100px]
+            transition-all
+            duration-300
+          "
+        >
+          <img
+            src="../assets/profile/bg.jpeg"
+            class="
+              w-full
+              h-full
+              object-cover
+              opacity-40
+              group-hover:opacity-60
+              transition
+              duration-300
+            "
+            alt=""
+          />
+          <img
+            src="../assets/profile/학교장님.png"
+            class="
+              object-cover
+              absolute
+              w-[160px]
+              bottom-0
+              opacity-0
+              group-hover:opacity-100
+              transform
+              translate-y-3/4
+              group-hover:translate-y-0
+              transition
+              duration-500
+            "
+            alt=""
+          />
+          <h1
+            class="
+              absolute
+              top-1/2
+              transform
+              -translate-y-1/2
+              left-5
+              group-hover:left-52
+              text-white
+              font-bold
+              text-md
+              md:text-2xl
+              transition-all
+              duration-300
+            "
+          >
+            장원혁 학교장
+          </h1>
         </div>
-        <p class="text-left">God bless you!~ <br> 순전한 학교에 방문해주신 여러분을 주님의 이름으로 <br class="hidden md:inline-block">환영합니다^^/</p>
+
+        <!-- Body Card -->
+        <div
+          class="
+            min-h-[120px]
+            py-3
+            px-4
+            bg-white
+            rounded-b
+            relative
+            z-10
+            shadow
+            text-gray-600
+          "
+        >
+          <div class="font-semibold mb-1">
+            <p>순전한 교회 담임목사</p>
+            <p>Email: pure@church.kr</p>
+          </div>
+          <p class="text-left">
+            God bless you!~ <br />
+            순전한 학교에 방문해주신 여러분을 주님의 이름으로
+            <br class="hidden md:inline-block" />환영합니다^^/
+          </p>
+        </div>
       </div>
     </div>
   </div>
---
+++ src/components/ServiceTeam.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="containter mx-auto px-6 md:px-12 lg:px-8">
+    <div class="mb-12 space-y-4 text-center">
+      <h1 class="text-4xl font-bold text-gray-900 md:text-5xl">
+        섬기시는 분들
+      </h1>
+    </div>
+    <div class="py-20 grid gap-28 md:gap-12 md:grid-cols-3">
+      <!-- First Director Leader -->
+      <div
+        v-for="leader in leaders"
+        :key="leader.id"
+        class="border-t-4 space-y-8 group md:mb-20"
+      >
+        <div
+          class="
+            w-32
+            h-32
+            -mt-16
+            mx-auto
+            rounded-[2rem]
+            rotate-45
+            overflow-hidden
+          "
+        >
+          <img
+            :src="leader.img"
+            alt=""
+            loading="lazy"
+            class="
+              w-full
+              h-full
+              object-cover
+              -rotate-45
+              scale-125
+              mx-auto
+              transition
+              duration-300
+              group-hover:scale-[1.4]
+            "
+          />
+        </div>
+        <div class="text-center">
+          <h5 class="text-xl text-gray-800 font-semibold">{{ leader.name }}</h5>
+          <span class="text-gray-500"> {{ leader.role }} </span>
+        </div>
+        <a
+          :href="leader.link"
+          class="block w-max mx-auto text-center text-blue-500"
+          >학교 가기</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+
+export default {
+  setup() {
+    let leaders = [
+      {
+        id: 0,
+        name: "크루엘라",
+        img: "https://i.imgur.com/tJghX0Z.jpg",
+        role: "Director Leader",
+        link: "#home",
+      },
+      {
+        id: 1,
+        name: "블랙 위도우",
+        img: "https://i.imgur.com/kjiTOzB.gif",
+        role: "Director Leader",
+        link: "#prayer",
+      },
+      {
+        id: 2,
+        name: "해리 포터",
+        img: "https://cdnweb01.wikitree.co.kr/webdata/editor/202008/24/img_20200824081913_996d6f13.webp",
+        role: "Director Leader",
+        link: "#kar",
+      },
+      {
+        id: 3,
+        name: "닥터 스트레인저",
+        img: "https://pbs.twimg.com/media/Cv6xICqWIAAPHOf?format=jpg&name=small",
+        role: "Director Leader",
+        link: "#kar",
+      },
+      {
+        id: 4,
+        name: "헤르미온느 진 그레인저",
+        img: "https://cdnweb01.wikitree.co.kr/webdata/editor/202006/18/img_20200618133404_50940b22.webp",
+        role: "Director Leader",
+        link: "#kar",
+      },
+    ];
+    return {
+      leaders,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
