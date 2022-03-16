eb32786158b6d8df6fc8ef829a729eb7f816d468
parkyoungmean <PYM@bag-yeongmin-ui-MacBook-Pro.local>
2022-03-16 22:32:15 +0900

    05-01 make schools section

--- src/components/SimpleHome.vue
+++ src/components/SimpleHome.vue
@@ -7,9 +7,29 @@
     <ProfileCard />
     <ServiceTeam />
   </section>
-
-  <section id="school" class="min-h-screen"></section>
-
+  <!-- pure school introduction -->
+  <section id="school" class="grid place-items-center min-h-screen">
+    <!-- Responsive Grid Layout -->
+    <div class="p-4 py-20 max-w-5xl grid gap-4 xs:grid-cols-2 xs:p-8 md:grid-cols-4">
+      <h1 class="text-4xl font-extrabold xs:col-span-2 xs:grid xs:gap-4 xs:grid-cols-2 md:col-span-3 md:text-5xl md:grid-cols-3">
+        <span class="md-span-2">순전한 학교</span>
+      </h1>
+      <p class="xs:row-start-2 xs:col-start-2 xs:self-center md:col-start-1 md:col-span-2 md:pr-12 md:text-lg">
+        순전한 학교는 능력있는 중보기도, 치유학교, 복음학교, 사역자 학교, 친밀한
+        학교의 커리큘럼으로 구성됩니다.
+      </p>
+      <div class="h-16 bg-blue-500 xs:h-auto xs:square"></div>
+      <div class="h-16 bg-blue-500 xs:h-auto xs:square"></div>
+      <div class="h-16 bg-pink-500 xs:h-auto xs:square"></div>
+      <div class="h-16 bg-blue-500 xs:h-auto xs:square col-start-2"></div>
+      <div class="h-16 bg-pink-500 xs:h-auto xs:square"></div>
+      <div class="h-16 bg-blue-500 xs:h-auto xs:square"></div>
+      <div class="h-16 bg-blue-500 xs:h-auto xs:square"></div>
+      <div class="h-16 bg-pink-500 xs:h-auto xs:square"></div>
+      <p class="self-center md:text-lg md:col-span-2 md:text-center md:px-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius tenetur temporibus nihil perspiciatis iure ducimus repellendus maiores. Beatae quia corrupti consequatur modi est, nam, voluptatum natus voluptatibus inventore minima sed.</p>
+    </div>
+  </section>
+  <!-- notices & donation -->
   <section id="notices" class="min-h-screen bg-green-100">
     <div class="w-full min-h-screen font-sans text-gray-900">
       <main class="md:py-16 px-4 mx-auto max-w-7xl">
@@ -38,16 +58,34 @@
         <!-- Tabs -->
         <div x-data="{ tab: 'notice' }" class="">
           <div class="flex gap-x-8 items-center pt-12">
-              <a href="" x-on:click.prevent="tab = 'notice'" class="text-sm md:text-lg font-medium"> 공지사항 </a>
-              <a href="" x-on:click.prevent="tab = 'bbs'" class="text-sm md:text-lg font-indigo-500">
-                자유 게시판
-              </a>
-              <a href="" x-on:click.prevent="tab = 'gallery'" class="text-sm md:text-lg font-indigo-500">
-                활동 사진
-              </a>
-              <a href="" x-on:click.prevent="tab = 'donation'" class="text-sm md:text-lg font-indigo-500">
-                후원 안내
-              </a>
+            <a
+              href=""
+              x-on:click.prevent="tab = 'notice'"
+              class="text-sm md:text-lg font-medium"
+            >
+              공지사항
+            </a>
+            <a
+              href=""
+              x-on:click.prevent="tab = 'bbs'"
+              class="text-sm md:text-lg font-indigo-500"
+            >
+              자유 게시판
+            </a>
+            <a
+              href=""
+              x-on:click.prevent="tab = 'gallery'"
+              class="text-sm md:text-lg font-indigo-500"
+            >
+              활동 사진
+            </a>
+            <a
+              href=""
+              x-on:click.prevent="tab = 'donation'"
+              class="text-sm md:text-lg font-indigo-500"
+            >
+              후원 안내
+            </a>
           </div>
           <div class="">
             <div x-show="tab === 'notice'" class="">
