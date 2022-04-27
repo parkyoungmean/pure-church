219b9e233a59d8410f039c4d619c2e7ef9ab5720
parkyoungmean <PYM@bag-yeongmin-ui-MacBook-Pro.local>
2022-04-28 02:31:15 +0900

    09-02 UsersView: import & use users data from backend server with AXIOS

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -34,7 +34,7 @@
                     <!-- AVATAR -->
                     <div class="mr-4 relative w-14">
                       <img
-                        :src="user.img"
+                        :src="user.Img"
                         alt=""
                         class="rounded-full w-14 h-14 mr-2"
                       />
@@ -60,14 +60,14 @@
                             dark:text-gray-300
                           "
                         >
-                          {{ user.name }}
+                          {{ user.Name }}
                         </div>
                         <div class="text-gray-700 dark:text-gray-600 text-xs">
-                          등록일: {{ user.createdAt }}
+                          등록일: {{ user.CreatedAt }}
                         </div>
                       </div>
                       <div class="text-gray-400 text-sm dark:text-gray-600">
-                        {{ user.extraInfo }}
+                        {{ user.Greetings }}
                       </div>
                     </div>
                   </div>
@@ -94,7 +94,18 @@
             "
           >
             <!-- CONTENT -->
-            <div id="content" class="flex-1 overflow-y-scroll p-5 scrollbar-thumb-color dark:scrollbar-thumb-color-dark scrollbar-with space-y-5">
+            <div
+              id="content"
+              class="
+                flex-1
+                overflow-y-scroll
+                p-5
+                scrollbar-thumb-color
+                dark:scrollbar-thumb-color-dark
+                scrollbar-with
+                space-y-5
+              "
+            >
               <div>
                 <!-- AVARTA -->
                 <div class="flex flex-col items-center mb-5">
@@ -139,49 +150,98 @@
                     등록일: 2016년 03월 21일
                   </p>
                   <p class="text-sm tracking-widest text-gray-400">
-                    안녕 자기? 보여줄게, 내가 누군지 난 원래부터 뛰어나고, 못됐고, 약간 돌았지
+                    안녕 자기? 보여줄게, 내가 누군지 난 원래부터 뛰어나고,
+                    못됐고, 약간 돌았지
                   </p>
                 </div>
               </div>
               <div class="mb-5 pt-5 px-10 text-gray-800 dark:text-gray-400">
                 <!-- 연락처 -->
-                <h1 class="uppercase tracking-widst text-lg font-bold">연락처</h1>
-                <hr class="mb-5 w-1/6">
+                <h1 class="uppercase tracking-widst text-lg font-bold">
+                  연락처
+                </h1>
+                <hr class="mb-5 w-1/6" />
                 <h1>010-10000-0001</h1>
                 <h1>loremipsu@gmail.com</h1>
                 <h1>영국, 런던</h1>
-                <hr class="my-10">
+                <hr class="my-10" />
                 <!-- 관계 -->
                 <h1 class="uppercase tracking-widst text-lg font-bold">관계</h1>
-                <hr class="mb-5 w-1/6">
+                <hr class="mb-5 w-1/6" />
                 <div class="flex -space-x-6">
-                  <img src="http://t1.daumcdn.net/thumb/C210x210.fjpg.q75/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fcontentshub%2Fsdb%2F03e1cae25bba9e22ef3cb6935201ccf765c657fab9675eea94d2b79738a60742" alt="호레이스 바둔" class="inline-block w-16 h-16 rounded-full border-4 border-gray-200 transition duration-300 hover:translate-y-2">
-                  <img src="https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Fcineplay%2F0004751349384bb0ab3d969203cf870d.jpg" alt="아니타 달링" class="inline-block w-16 h-16 rounded-full border-4 border-gray-200 transition duration-300 hover:translate-y-2">
-                  <img src="https://img.khan.co.kr/news/2021/05/26/l_2021052601002985500248376.webp" alt="바로니스" class="inline-block w-16 h-16 rounded-full border-4 border-gray-200 transition duration-300 hover:translate-y-2">
+                  <img
+                    src="http://t1.daumcdn.net/thumb/C210x210.fjpg.q75/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fcontentshub%2Fsdb%2F03e1cae25bba9e22ef3cb6935201ccf765c657fab9675eea94d2b79738a60742"
+                    alt="호레이스 바둔"
+                    class="
+                      inline-block
+                      w-16
+                      h-16
+                      rounded-full
+                      border-4 border-gray-200
+                      transition
+                      duration-300
+                      hover:translate-y-2
+                    "
+                  />
+                  <img
+                    src="https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Fcineplay%2F0004751349384bb0ab3d969203cf870d.jpg"
+                    alt="아니타 달링"
+                    class="
+                      inline-block
+                      w-16
+                      h-16
+                      rounded-full
+                      border-4 border-gray-200
+                      transition
+                      duration-300
+                      hover:translate-y-2
+                    "
+                  />
+                  <img
+                    src="https://img.khan.co.kr/news/2021/05/26/l_2021052601002985500248376.webp"
+                    alt="바로니스"
+                    class="
+                      inline-block
+                      w-16
+                      h-16
+                      rounded-full
+                      border-4 border-gray-200
+                      transition
+                      duration-300
+                      hover:translate-y-2
+                    "
+                  />
                 </div>
-                <hr class="my-10">
+                <hr class="my-10" />
                 <!-- 교육 -->
                 <h1 class="uppercase tracking-widst text-lg font-bold">교육</h1>
-                <hr class="mb-5 w-1/6">
-                <h1 class="text-sm font-semibold tracking-wider uppercase">VOL11기 학교</h1>
+                <hr class="mb-5 w-1/6" />
+                <h1 class="text-sm font-semibold tracking-wider uppercase">
+                  VOL11기 학교
+                </h1>
                 <h1 class="text-sm">
                   <span class="italic">도봉 순전한 교회</span>,
                   <span class="italic text-sm">수료</span> / 2016.4-2016.6
                 </h1>
-                <br>
-                <h1 class="text-sm font-semibold tracking-wider uppercase">강남 치유학교</h1>
+                <br />
+                <h1 class="text-sm font-semibold tracking-wider uppercase">
+                  강남 치유학교
+                </h1>
                 <h1 class="text-sm">
                   <span class="italic">강남 하나님아버지 교회</span>,
                   <span class="italic text-sm">수료</span> / 2020-2020
                 </h1>
-                <br>
-                <h1 class="text-sm font-semibold tracking-wider uppercase">치유학교 5기</h1>
+                <br />
+                <h1 class="text-sm font-semibold tracking-wider uppercase">
+                  치유학교 5기
+                </h1>
                 <h1 class="text-sm">
                   <span class="italic">온라인</span>,
-                  <span class="italic text-sm text-green-400">진행중</span> / 2022-2022
+                  <span class="italic text-sm text-green-400">진행중</span> /
+                  2022-2022
                 </h1>
-                <br>
-                <hr class="my-10">
+                <br />
+                <hr class="my-10" />
               </div>
             </div>
             <!-- END OF CONTENT -->
@@ -194,68 +254,21 @@
 </template>
 
 <script>
+/* eslint-disable */
+import axios from "axios";
+import { onMounted } from "@vue/runtime-core";
+
 export default {
   setup() {
-    let users = [
-      {
-        id: 0,
-        name: "크루엘라",
-        img: "https://i.imgur.com/tJghX0Z.jpg",
-        extraInfo: "try as I might I never was. I'm Cruella,",
-        createdAt: "2016.03.21",
-        bookmark: "#home",
-      },
-      {
-        id: 1,
-        name: "블랙 위도우",
-        img: "https://i.imgur.com/kjiTOzB.gif",
-        extraInfo: "나는 사람을 실수만으로 판단하지 않아.",
-        createdAt: "2022.04.22",
-        bookmark: "#prayer",
-      },
-      {
-        id: 2,
-        name: "곰돌이 푸",
-        img: "https://pbs.twimg.com/profile_images/985648888282411010/BRzwE67S_400x400.jpg",
-        extraInfo: "잘 찾아보면 행복한 일은 매일 있어♡",
-        createdAt: "2022.04.22",
-        bookmark: "#prayer",
-      },
-      {
-        id: 3,
-        name: "해리 포터",
-        img: "https://cdnweb01.wikitree.co.kr/webdata/editor/202008/24/img_20200824081913_996d6f13.webp",
-        extraInfo: "기억해, 네게 친구들이 있어. 혼자가 아니야.",
-        createdAt: "2022.04.21",
-        bookmark: "#kar",
-      },
-      {
-        id: 4,
-        name: "닥터 스트레인저",
-        img: "https://pbs.twimg.com/media/Cv6xICqWIAAPHOf?format=jpg&name=small",
-        extraInfo:
-          "너만의 볼품 없는 모습을 창조하라고, 너만의, 너만의 세상을 만들라고.",
-        createdAt: "2022.04.25",
-        bookmark: "#kar",
-      },
-      {
-        id: 5,
-        name: "피노키오",
-        img: "https://pbs.twimg.com/profile_images/1346360049518579712/k_Kl9lnV_400x400.jpg",
-        extraInfo:
-          "사랑이란, 다른 사람의 행복이 나의 행복을 결정짓는 아름다운 현상이라구!",
-        createdAt: "2022.04.26",
-        bookmark: "#kar",
-      },
-      {
-        id: 6,
-        name: "공유",
-        img: "https://i.imgur.com/RbBFZkQ.png",
-        extraInfo: "그 아이가 날 죽게 할 수 있는데 그 아이가 날 살게 해",
-        createdAt: "2022.04.26",
-        bookmark: "#kar",
-      },
-    ];
+    const { ref } = require("vue");
+    const users = ref([]);
+
+    onMounted(async () => {
+      await axios.get("http://localhost:4000/users").then((response) => {
+        users.value = response.data;
+        console.log(this.users);
+      });
+    });
 
     return {
       users,
